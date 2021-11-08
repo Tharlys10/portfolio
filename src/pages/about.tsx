227c069c2f926867212f24c6489684a03be73f31
tharlys10 <tharlys2015c@gmail.com>
2021-11-08 16:06:07 -0300

    feat: :sparkles: implentation carts

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,26 +8,104 @@ export default function About(){
 
   return (
     <div className={styles.container}>
-      <Image src="/images/about.png" alt="About" width={650} height={500}/>
+      <div className={styles.contentInformation}>
+        <Image src="/images/about.png" alt="About" width={650} height={500}/>
 
-      <div className={styles.information}>
-        <h1><span>&lt;</span>Tharlys Alves de Almeida<span>/&gt;</span></h1>
+        <div className={styles.information}>
+          <h1><span>&lt;</span>Tharlys Alves de Almeida<span>/&gt;</span></h1>
+        
+          <p>
+            Tenho {my_age} anos, sou de São Miguel, interior do estado do 
+            Rio Grande do Norte. Comecei a programar em 2017, no curso técnico 
+            em informatica na E.E.E.P Professora Maria Celia Pinheiro Falcão, 
+            que conclui em dezembro de 2019. Atualmente acadêmico em Análise e 
+            Desenvolvimento de Sistema na Estácio e um alumni da Rocketseat 
+            e atuando como desenvolvedor full stack na Brisanet Telecomunicações S.A.
+          </p>
+
+          <p>
+            Sou apaixonado por desenvolvimento web, pricipalmente em 
+            tecnologias que utilizam javascript, como ReactJS (NextJS), VueJS, 
+            NodeJS e NestJS. Sempre estou em busca de novos desafios e 
+            conhecimentos, seja em cursos ou em treinamentos.
+          </p>
+        </div>
+      </div>
       
-        <p>
-          Tenho {my_age} anos, sou de São Miguel, interior do estado do 
-          Rio Grande do Norte. Comecei a programar em 2017, no curso técnico 
-          em informatica na E.E.E.P Professora Maria Celia Pinheiro Falcão, 
-          que conclui em dezembro de 2019. Atualmente acadêmico em Análise e 
-          Desenvolvimento de Sistema na Estácio e um alumni da Rocketseat 
-          e atuando como desenvolvedor full stack na Brisanet Telecomunicações S.A.
-        </p>
+      <div className={styles.cart}>
+      <div className={styles.cartInformation}>
+          <h2>Habilidades</h2>
+          <span>
+            Tecnologias que mais utilizo no
+            meu ambiente profissional e nos
+            meus estudos.
+          </span>
+        </div>
 
-        <p>
-          Sou apaixonado por desenvolvimento web, pricipalmente em 
-          tecnologias que utilizam javascript, como ReactJS (NextJS), VueJS, 
-          NodeJS e NestJS. Sempre estou em busca de novos desafios e 
-          conhecimentos, seja em cursos ou em treinamentos.
-        </p>
+        <div className={styles.cartIcons}>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+        </div>
+      </div>
+
+      <div className={styles.cart}>
+        <div className={styles.cartIcons}>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+          <div className={styles.cartMin}></div>
+        </div>
+
+        <div className={styles.cartInformation}>
+          <h2>Ferramentas</h2>
+          <span>
+            Principais ferramentas que utilizo
+            no desenvolvimento de novos 
+            projetos.
+          </span>
+        </div>
       </div>
     </div>
   )
